a04b48de35b6364455ba8037d988fb2016d2b4b6
Consuelo Leal <consuelo.leal@outlook.com>
2019-05-16 09:00:41 -0400

    Add LOL version

--- src/data.js
+++ src/data.js
@@ -1,10 +1,56 @@
 /* Manejo de data */
+const myData = LOL.data;
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+const ourData = Object.values(LOL.data);
 
-const example = () => {
-  return 'example';
+const filterData = (data, condition) => {
+  for (var key in condition) {
+    if (condition[key] === "All")
+      delete condition[key]
+  }
+  return data.filter(function(champion) {
+    for (var key in condition) {
+      if (champion[key] === undefined || !champion[key].includes(condition[key]))
+        return false;
+    }
+    return true;
+  });
 };
 
-window.example = example;
+const sortData = (championData, sortBy, sortOrder) => {
+  let ordenaChampions = championData;
+
+  if (sortOrder == "a-z"){
+    ordenaChampions.sort((a,b)=> {
+      if (a[sortBy] < b[sortBy]) {return -1;}
+      if (a[sortBy] > b[sortBy]) {return  1;}
+      return 0;
+    })
+  }
+
+  if (sortOrder == "z-a"){
+    ordenaChampions.sort((a,b)=> {
+      if (a[sortBy] > b[sortBy]) {return -1;}
+      if (a[sortBy] < b[sortBy]) {return  1;}
+      return 0;
+    })
+  }
+
+  return ordenaChampions;
+};
+
+const computeStats = (data) => {
+  var hpAverage, maxHp, apAverage, maxAp;
+  var healthPoints = data.map(champion => champion["stats"]["hp"]);
+  var attackPoints = data.map(champion => champion["info"]["attack"]);
+  hpAverage = (healthPoints.reduce(function(a, b){ return a + b; }) / data.length).toFixed(2);
+  apAverage = (attackPoints.reduce(function(a, b){ return a + b; }) / data.length).toFixed(2);
+  maxHp = Math.max(...healthPoints);
+  maxAp = Math.max(...attackPoints);
+
+  return {Salud_Promedio: hpAverage, Ataque_Promedio: apAverage, Salud_Máxima: maxHp, Ataque_Máximo: maxAp};
+};
+
+window.filterData = filterData;
+window.sortData = sortData;
+window.computeStats = computeStats;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,74 @@
-/* Manejo del DOM */
+const rootNode     = document.getElementById('root');
+const rootStatNode = document.getElementById('stats');
+var championStats, filteredData;
+
+document.addEventListener("DOMContentLoaded", load, false);
+
+function load(){
+  loadChampion(ourData);
+  championStats = window.computeStats(ourData);
+  loadStats(championStats);
+  selectTag.onchange = function(e) {
+    e.preventDefault();
+    filteredData = window.filterData(ourData, {tags: selectTag.value});
+    removeAll();
+    if (selectSortBy.value !== "Normal")
+      filteredData = sortData(filteredData, "name", selectSortBy.value)
+    loadChampion(filteredData);
+    championStats = window.computeStats(filteredData);
+    loadStats(championStats);
+  }
+  selectSortBy.onchange = function(e){
+    e.preventDefault();
+    filteredData = window.filterData(ourData, {tags: selectTag.value});
+    removeAll();
+    if (selectSortBy.value !== "Normal")
+      filteredData = sortData(filteredData, "name", selectSortBy.value)
+    loadChampion(filteredData);
+    championStats = window.computeStats(filteredData);
+    loadStats(championStats);
+  }
+}
+
+function removeAll(){
+  while (rootNode.firstChild) {
+    rootNode.removeChild(rootNode.firstChild);
+  }
+  while (rootStatNode.firstChild) {
+    rootStatNode.removeChild(rootStatNode.firstChild);
+  }
+}
+
+
+function loadStats(stats){
+  for (var key in stats){
+    const statNode = document.createElement("div");
+    statNode.className = "statNode col-sm-3";
+    statNode.innerHTML = "<span class='statLabel'>" + key.replace("_", " ") + ":</span> " + stats[key];
+    rootStatNode.appendChild(statNode);
+  }
+};
+
+function loadChampion(data){
+  data.forEach(function(champion) {
+    const championNode = document.createElement("div");
+
+    championNode.id = champion.name;
+    championNode.className = "championNode col-sm-3";
+    champion.tags.forEach(function(championTag){
+      championNode.className += " " + championTag;
+    });
+    championNode.innerHTML =  '<div class="card" style="width: 15rem;">' + 
+                                '<img id="image"src="' + champion.img + '"></img>' + 
+                                '<div class="card-body">' + 
+                                  '<h5 class="card-title">' + champion.key + " - " + champion.name + '</h5>' + 
+                                  // '<span class="card-span">Altura: </span><em>' + champion.height + '</em></br>' +
+                                  // '<span class="card-span">Peso: </span><em>' + champion.weight + '</em></br>' +
+                                  '<span class="card-span">Tipo: </span><em>' + champion.tags + '</em></br>' +
+                                  // '<span class="card-span">Debilidad: </span><em>' + champion.weaknesses + '</em></br>' +
+                                  // '<span class="card-span">Eclosión: </span><em>' + champion.egg + '</em></br>' +
+                                '</div>' + 
+                              '</div>';
+    rootNode.appendChild(championNode);
+  });
+}
